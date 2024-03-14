12b6a2483583e3594ecc07e80e020a410b575979
rogerdanielss <danielsimoesp@outlook.com>
2024-03-13 22:52:32 -0300

    add: ranges tests

---
+++ example-rspec-tdd/spec/matchers/ranges/ranges_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+describe (1..5), 'Ranges' do
+  it '#cover' do
+    expect(subject).to cover(2)
+    is_expected.to cover(2, 5)
+    expect(subject).not_to cover(0, 6)
+  end
+
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2, 5) }
+  it { is_expected.not_to cover(0, 6) }
+
+  it { expect(subject).to cover(2) }
+end
