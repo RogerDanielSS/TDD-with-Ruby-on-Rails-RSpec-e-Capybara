ac900b45a13abe93df445d02c281c0988f19174c
rogerdanielss <danielsimoesp@outlook.com>
2024-03-14 23:23:35 -0300

    add: helper methods

--- example-rspec-tdd/spec/matchers/composicao/composicao_spec.rb
+++ example-rspec-tdd/spec/matchers/composicao/composicao_spec.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
+# require_relative '../../helpers/helpers'
+
+# RSpec.configure do |config|
+#   config.include Helper
+# end
+
 describe 'Ruby on Rails' do
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
   it { expect(frutas).to eq('banana').or eq('laranja').or eq('uva') }
 
-  def frutas
-    %w[banana laranja uva].sample
-  end
+  # helper method arbitrário
+  # def frutas
+  #   %w[banana laranja uva].sample
+  # end
 end
--- example-rspec-tdd/spec/string/string_spec.rb
+++ example-rspec-tdd/spec/string/string_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'string_not_empty'
 
 describe String do
